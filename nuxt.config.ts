75ce522e71e97a50b734f688ea5a7724daff098e
DAI JIE <daijie@keqin.tech>
2023-07-19 17:14:44 +0800

    build: publish to cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+  nitro: {
+    "preset": "cloudflare"
+  },
   css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/content',
