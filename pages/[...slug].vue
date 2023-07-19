a2e08e2a67161e7a900b9197c13e74773c3e078b
DAI JIE <daijie@keqin.tech>
2023-07-19 22:43:38 +0800

    feat: home page & detail's page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,16 +1,26 @@
 <template>
-    <main class="p-10 max-w-3xl text-lg text-zinc-900 bg-white leading-relaxed whitespace-pre-line ">
-      <ContentDoc />
-    </main>
+  <div class="app">
+    <div class="max-w-7xl m-auto flex">
+      <main class="flex gap-4 w-full max-w-3xl text-zinc-900 whitespace-pre-line ">
+        <ContentDoc class="p-10 leading-relaxed bg-neutral-50" :class="`text-${fontSize}`" />
+        <div class="relative mb-3">
+          <div class="fixed bottom-3 bg-white rounded-full shadow">
+            <SettingsFontSize :default-value="fontSize" @change="(value) => fontSize = value" />
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
 <script setup lang="ts">
 
 const { page } = useContent()
 useContentHead(page)
+const fontSize = ref('lg')
 </script>
 <style scoped>
-body {
-  @apply bg-neutral-50;
+.app {
+  @apply bg-stone-200;
 }
 :deep(p) {
   @apply py-2;
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,46 @@
 <template>
-    <div class="h-screen p-10 max-w-7xl m-auto">
-        <NuxtLink to="/" class="flex my-16">
-            <img src="/icon.png" class="w-14 h-14 " />
-            <div class="ml-4">
-                <h1 class="text-3xl font-black text-slate-900 songti">佛典电子化计划</h1>
-                <p class="description">用现代 Web 技术重排佛典</p>
-            </div>
-        </NuxtLink>
-        <ul class="flex">
-            <li>
-                <NuxtLink class="flex hover:bg-stone-50 p-2 rounded" to="/xifangquezhi">
-                    <img class="w-16" src="/cover/xifangquezhi.jpg" />
-                    <div class="p-3">
-                        <span class="font-semibold text-base mb-2 block">西方确指</span>
-                        <p class="block text-gray-400 whitespace-pre-line text-[12px]">觉明妙行菩萨说
-                        菩萨戒弟子常摄集</p>
-                        <p class="text-gray-400 whitespace-pre-line text-[12px]">庐山东林寺印经处</p>
-                    </div>
-                </NuxtLink>
-            </li>
-        </ul>
-        <footer class="bottom-0 absolute text-[13px] p-5 text-gray-500">
-            <ul>
-                <li>* 本站是开源项目 <NuxtLink to="https://github.com/shiny/sutta/"><Icon name="bxl:github" />佛典电子化计划</NuxtLink>，与各地的护法寺均不存在任何关系</li>
-                <li>&copy; 2023 GPL-3.0 license </li>
+    <div class="app">
+        <div class="py-10 max-w-7xl m-auto">
+            <NuxtLink to="/" class="flex my-16">
+                <img src="/icon.png" class="w-14 h-14 " />
+                <div class="ml-4">
+                    <h1 class="text-3xl font-black text-slate-900 songti">佛典电子化计划</h1>
+                    <p class="description">用现代 Web 技术重排佛典</p>
+                </div>
+            </NuxtLink>
+            <ul class="flex">
+                <li>
+                    <NuxtLink class="flex transition bg-stone-50 hover:bg-white hover:shadow p-3 rounded" to="/xifangquezhi">
+                        <img class="w-16" src="/cover/xifangquezhi.jpg" />
+                        <div class="p-3">
+                            <span class="font-semibold text-base mb-2 block">西方确指</span>
+                            <p class="block text-gray-400 whitespace-pre-line text-[12px]">觉明妙行菩萨说
+                            菩萨戒弟子常摄集</p>
+                            <p class="text-gray-400 whitespace-pre-line text-[12px]">庐山东林寺印经处</p>
+                        </div>
+                    </NuxtLink>
+                </li>
             </ul>
+        </div>
+        <footer class="mt-10 w-full border-t">
+            <section class="max-w-7xl m-auto text-[13px] py-5 text-gray-500">
+                <p class="text-sm kaiti text-zinc-600">
+                    <h3>回向偈</h3>
+                    愿以此功德，
+                    庄严佛净土。
+                    上报四重恩，
+                    下济三途苦。
+                    若有见闻者，
+                    悉发菩提心。
+                    尽此一报身，
+                    同生极乐国。
+                </p>
+                <ul class="mt-3">
+                    <li class="kaiti"></li>
+                    <li>* 本项目与各地的护法寺均不存在任何关系</li>
+                    <li>&copy; 2023 <NuxtLink to="https://github.com/shiny/sutta/"><Icon name="bxl:github" />佛典电子化计划</NuxtLink> GPL-3.0 license </li>
+                </ul>
+            </section>
         </footer>
     </div>
 </template>
@@ -35,8 +51,8 @@ useSeoMeta({
 </script>
 <style scoped>
 
-body {
-  @apply bg-neutral-50;
+.app {
+  @apply h-screen bg-[--background-color] bg-stone-100 relative;
 }
 .description {
     @apply text-gray-500 text-sm py-0 px-1;
