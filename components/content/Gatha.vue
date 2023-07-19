b493ed7ef83d8fd61772e6eb4dcd07f666927282
DAI JIE <daijie@keqin.tech>
2023-07-19 22:44:07 +0800

    feat: 偈言需要自动换行

--- components/content/Gatha.vue
+++ components/content/Gatha.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="text-center text-xl py-3 leading-relaxed kaiti">
+  <div class="text-center text-xl py-3 leading-relaxed kaiti whitespace-pre-line">
     <slot />
   </div>
 </template>
-  
