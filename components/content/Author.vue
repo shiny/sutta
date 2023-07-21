987537a3b69a9311f490d53202beeda01584be80
DaiJie <daijie@php.net>
2023-07-21 18:55:24 +0800

    fix: 作者字体自适应

--- components/content/Author.vue
+++ components/content/Author.vue
@@ -1,5 +1,10 @@
 <template>
-<div class="kaiti text-lg leading-5">
+<div class="author kaiti leading-tight">
 <slot></slot>
 </div>
 </template>
+<style scoped>
+.author {
+    font-size: 100%;
+}
+</style>
