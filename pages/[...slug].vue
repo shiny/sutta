eeed47d4744b091dfa74faf14d0ef51cc610dfae
DaiJie <daijie@php.net>
2023-07-21 18:56:34 +0800

    fix: 段落间距现在也能随字体大小调整了

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,7 +2,7 @@
   <div class="app">
     <div class="max-w-7xl m-auto flex">
       <main class="flex gap-4 w-full max-w-3xl text-zinc-900 whitespace-pre-line ">
-        <ContentDoc class="p-10 leading-relaxed bg-neutral-50" :class="`text-${fontSize}`" />
+        <ContentDoc class="p-10 leading-relaxed bg-neutral-50" :class="`text-${fontSize} paragraph-${fontSize}`" />
         <div class="relative mb-3">
           <div class="fixed bottom-3 bg-white rounded-full shadow">
             <SettingsFontSize :default-value="fontSize" @change="(value) => fontSize = value" />
@@ -22,7 +22,22 @@ const fontSize = ref('lg')
 .app {
   @apply bg-stone-200;
 }
-:deep(p) {
+:deep(.paragraph-sm p){
+  @apply py-1;
+}
+:deep(.paragraph-lg p){
+  @apply py-1.5;
+}
+:deep(.paragraph-xl p){
   @apply py-2;
+}
+:deep(.paragraph-2xl p){
+  @apply py-3;
+}
+:deep(.paragraph-3xl p){
+  @apply py-4;
+}
+:deep(.paragraph-4xl p){
+  @apply py-5;
 }
 </style>  
